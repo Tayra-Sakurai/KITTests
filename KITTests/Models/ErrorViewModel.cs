2807c450eedd23afa03e43034a9afda797951cc0
SAKURAI Tayra <taira_sakurai@outlook.jp>
2026-03-16 08:50:35 +0900

    プロジェクトファイルを追加。

---
+++ KITTests/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KITTests.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
